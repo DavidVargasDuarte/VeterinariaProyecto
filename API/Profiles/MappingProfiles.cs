6e2b81928b96a446a35def160306f49615c69c2c
DavidVargasDuarte <jaurevargastorres@gmail.com>
2023-10-19 18:55:51 -0500

    feat: :sparkles: Creacion del perfil de mapeo entre entidades y las clases de DTO

---
+++ API/Profiles/MappingProfiles.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dto;
+using AutoMapper;
+using Dominio;
+
+namespace API.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Citas, CitaDto>().ReverseMap();
+            CreateMap<DetalleMovimiento, DetallemovimientoDto>().ReverseMap();
+            CreateMap<Especies, EspecieDto>().ReverseMap();
+            CreateMap<Laboratorios, LaboratorioDto>().ReverseMap();
+            CreateMap<Mascotas, MascotaDto>().ReverseMap();
+            CreateMap<Medicamento, MedicamentoDto>().ReverseMap();
+            CreateMap<MedicamentoProveedores, MedicamentoProveedorDto>().ReverseMap();
+            CreateMap<MovimientoMedic, MovimientoMedicDto>().ReverseMap();
+            CreateMap<Propietario, PropietarioDto>().ReverseMap();
+            CreateMap<Proveedores, ProveedorDto>().ReverseMap();
+            CreateMap<Razas, RazaDto>().ReverseMap();
+            CreateMap<Roles, RolesDto>().ReverseMap();
+            CreateMap<Roles, RolesDto>().ReverseMap();
+            CreateMap<RolUsuarios, RolesDto>().ReverseMap();
+            CreateMap<TipoMovimineto, TipoMovimientoDto>().ReverseMap();
+            CreateMap<TratamientosMedicos, TratamientoMedicoDto>().ReverseMap();
+            CreateMap<Usuarios, UsuarioDto>().ReverseMap();
+            CreateMap<Veterinario, VeterinarioDto>().ReverseMap();
+
+        }
+    }
+}
