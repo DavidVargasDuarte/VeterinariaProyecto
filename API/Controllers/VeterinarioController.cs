cbc31a47709b12c7a2ef9810de3ffadfe9c3d4fe
DavidVargasDuarte <jaurevargastorres@gmail.com>
2023-10-19 18:57:56 -0500

    feat: :sparkles: Controlador y clase por defecto en la creacion del proyecto WEBAPI

---
+++ API/Controllers/VeterinarioController.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dto;
+using AutoMapper;
+using Dominio;
+using Dominio.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers;
+[ApiVersion("1.0")]
+[ApiVersion("1.1")]
+
+
+public class VeterinarioController : BaseApiController
+{
+    private readonly IUnitOfwork unitOfWork;
+    private readonly IMapper mapper;
+
+    public VeterinarioController(IUnitOfwork unitOfWork, IMapper mapper)
+    {
+        this.unitOfWork = unitOfWork;
+        this.mapper = mapper;
+    }
+
+    [HttpGet]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<IEnumerable<VeterinarioDto>>> Get()
+    {
+        var datos = await unitOfWork.Cita.GetAllAsync();
+        return mapper.Map<List<VeterinarioDto>>(datos);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<VeterinarioDto>> Get(int id)
+    {
+        var data = await unitOfWork.Cita.GetByIdAsync(id);
+        if (data == null)
+        {
+            return NotFound();
+        }
+        return this.mapper.Map<VeterinarioDto>(data);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+    public async Task<ActionResult<VeterinarioDto>> Post(VeterinarioDto veterinarioDto)
+    {
+        var data = this.mapper.Map<Veterinario>(veterinarioDto);
+        this.unitOfWork.Veterinario.Add(data);
+        await unitOfWork.SaveAsync();
+        if (data == null)
+        {
+            return BadRequest();
+        }
+        veterinarioDto.Id = data.Id;
+        return CreatedAtAction(nameof(Post), new { id = veterinarioDto.Id }, veterinarioDto);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+    public async Task<ActionResult<VeterinarioDto>> Put(int id, [FromBody] VeterinarioDto veterinarioDto)
+    {
+        if (veterinarioDto == null)
+        {
+            return NotFound();
+        }
+        var data = this.mapper.Map<Veterinario>(veterinarioDto);
+        unitOfWork.Veterinario.Update(data);
+        await unitOfWork.SaveAsync();
+        return veterinarioDto;
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var data = await unitOfWork.Veterinario.GetByIdAsync(id);
+        if (data == null)
+        {
+            return NotFound();
+        }
+        unitOfWork.Veterinario.Remove(data);
+        await unitOfWork.SaveAsync();
+        return NoContent();
+    }
+}
